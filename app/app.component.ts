0d40e6c3a15d3d0c55cc6dfdd8fef3d0ba634e16
Cody Boyko <cody.boyko2@ontario.ca>
2016-08-30 10:17:01 -0400

    Add basic template and timer class.

--- app/app.component.ts
+++ app/app.component.ts
@@ -1,7 +1,33 @@
 import { Component } from '@angular/core';
 
+export class Timer {
+	totalTime: string;
+	hours: number;
+	minutes: number;
+	seconds: number;
+}
+
 @Component({
     selector: 'my-app',
-    template: '<h1>Meeting Timer</h1>'
+    template: `
+    	<h1>{{title}}</h1>
+    	<div>
+    		You have <span>{{timer.totalTime}}</span> left.
+    	</div>
+    	<div>
+    		<label for='total'>Total Time: </label>
+    		<input id="total" [(ngModel)]="timer.totalTime" placeholder="total time">
+    	</div>
+    	<button>Start/Stop</button>
+    	<button>Reset</button>
+    	`
 })
-export class AppComponent { }
+export class AppComponent {
+	title = "Meeting Timer";
+	timer: Timer = {
+		totalTime: '00:00:00',
+		hours: 0,
+		minutes: 0,
+		seconds: 0
+	};
+}
