a32a71a76f7c3d450e528ffac3de5669bc33381a
boykoc <codywboyko@gmail.com>
2017-09-20 13:27:09 -0400

    Capture time entered by user.
    Set cursor position to right of input value
    in time form. Under the right circumstances
    when a user tried to edit the time the cursor
    would be moved to the left and new values
    would not be captured properly.
    
    Upgraded Renderer to Renderer2.
    
    Fixes #2.

--- src/app/focus.directive.ts
+++ src/app/focus.directive.ts
@@ -1,13 +1,11 @@
-import { Directive, ElementRef, HostListener, Input, Renderer } from '@angular/core';
+import { Directive, ElementRef, HostListener, Input, Renderer2 } from '@angular/core';
 @Directive({
   selector: '[myFocus]'
 })
 export class FocusDirective {
   private _defaultFocus = false;
-  constructor(private el: ElementRef, private renderer: Renderer) { }
-  // @Input() set defaultColor(giveFocus: boolean){
-  //   this._defaultFocus = giveFocus || this._defaultFocus;
-  // }
+  constructor(private el: ElementRef, private renderer: Renderer2) { }
+
   @Input('myFocus') giveFocus: boolean;
   @HostListener('changes') ngOnChanges() {
     this.focus(this.giveFocus || this._defaultFocus);
@@ -15,7 +13,10 @@ export class FocusDirective {
 
   private focus(focused: boolean) {
     if (focused) {
-      this.renderer.invokeElementMethod(this.el.nativeElement, 'focus', []);
+      let cursor_position = this.el.nativeElement.value.length;
+      this.el.nativeElement.focus();
+      // Ensure cursor position is at end of input.
+      this.el.nativeElement.setSelectionRange(cursor_position, cursor_position);
     }
   }
 }
